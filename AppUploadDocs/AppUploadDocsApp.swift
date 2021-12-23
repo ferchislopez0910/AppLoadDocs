b540bf7754f637e109a45ae95f2f88050637f21c
ferchislopez0910 <ferchislopez0910@gmail.com>
2021-12-22 21:16:06 -0500

    Created apliaction Swift Load docs, this app its function ok with use Firebase

---
+++ AppUploadDocs/AppUploadDocsApp.swift
@@ -0,0 +1,35 @@
+//
+//  AppUploadDocsApp.swift
+//  AppUploadDocs
+//
+//  Created by Maria Fernanda Lopez on 21/12/21.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct AppUploadDocsApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            let viewModel = AppViewModel()
+            ContentView()
+                .environmentObject(viewModel)
+        }
+    }
+}
+
+class AppDelegate: NSObject,  UIApplicationDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+        [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+    return true
+    }
+
+    
+}
---
+++ Pods/Target Support Files/Pods-AppUploadDocs-AppUploadDocsUITests/Pods-AppUploadDocs-AppUploadDocsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppUploadDocs_AppUploadDocsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AppUploadDocs_AppUploadDocsUITests
+@end
